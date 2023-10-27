9d36735a9fa742680bc00de830f9a391beebfc46
Monique Rodriguez <moniquerdz@gmail.com>
2023-10-27 15:24:08 -0500

    added dot env dependency and mongoose

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
+
+// Replace with your MongoDB connection string
+const db = 'YOUR_MONGODB_CONNECTION_STRING';  
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // Route
 app.get('/', (req, res) => res.send('API Running'));
